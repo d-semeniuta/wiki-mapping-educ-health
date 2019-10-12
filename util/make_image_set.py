583f9667cc0aa1e4191028b90cac791ade934250
Riley16 <dashags123456789@gmail.com>
2019-10-12 02:17:36 -0700

    test

--- util/make_image_set.py
+++ util/make_image_set.py
@@ -43,14 +43,33 @@ if not raster:
 geo_prop = GeoProps()
 geo_prop.import_geogdal(raster)
 
-# assume clusters are identical across different surveys
-unique_DHS_clusters = imr_5yr.drop_duplicates(['lat','lon'])
-# current nightlights data set only covers Africa, must limit nightlights queries to coordinates within Africa
-unique_DHS_clusters_Africa = unique_DHS_clusters.query('(lat < 37.3408) '
-                                                       '& (lon > -25.360996)'
-                                                       '& (lat > -34.821334)'
-                                                       '& (lon < 63.4954491)'
-                                                       '& (lon > 55)')
+# IMR unique measurements: 125261, 85006 with imr 0, 30979 in Africa, 17784 with imr 0 in Africa
+# unique clusters: 42092 total, 10507 in Africa
+# 25 countries total, 15 in Africa
+
+# # assume clusters are identical across different surveys
+# # unique_DHS_clusters = imr_5yr.drop_duplicates(['lat','lon'])
+# unique_DHS_clusters = imr_5yr
+# # current nightlights data set only covers Africa, must limit nightlights queries to coordinates within Africa
+# unique_DHS_clusters_Africa = unique_DHS_clusters.query('(lat < 37.3408) '
+#                                                        '& (lon > -25.360996)'
+#                                                        '& (lat > -34.821334)'
+#                                                        '& (lon < 63.4954491)')
+# print()
+# print('Africa:')
+# num_rows = len(unique_DHS_clusters_Africa)
+# num_rows_0imr = len(unique_DHS_clusters_Africa[unique_DHS_clusters_Africa.imr == 0])
+# print('Number of cluster measurements:', num_rows)
+# print('Number of cluster measurements with imr 0:', num_rows_0imr)
+# print('Percentage:', num_rows_0imr / num_rows)
+# print('Number of unique clusters:', unique_DHS_clusters_Africa['cluster_id'].nunique())
+# print('Number of countries surveyed:', unique_DHS_clusters_Africa['country'].nunique())
+
+# print("Unique cluster measurements in Africa:")
+# print(len(unique_DHS_clusters_Africa))
+# # print(unique_DHS_clusters_Africa)
+# print('Number of unique clusters:', unique_DHS_clusters_Africa['cluster_id'].nunique())
+# print('Number of countries surveyed:', unique_DHS_clusters_Africa['country'].nunique())
 
 mean_intensities = []
 std_intensities = []
@@ -58,20 +77,21 @@ std_intensities = []
 # unique_DHS_clusters_Africa = unique_DHS_clusters_Africa.head(n=2)
 # print(unique_DHS_clusters_Africa['cluster_id'].nunique())
 # 10507 unique clusters in Africa
-for idx, cluster in unique_DHS_clusters_Africa.iterrows():
-    # print(cluster)
-    raster_img = geo_prop.get_coord_centered_img(cluster['lat'], cluster['lon'], 5, 5, raster,
-                                                 filepath=data_dir + '/nightlights/' + cluster['cluster_id'] + '.png')
 
-    mean_intensities.append(float(np.mean(raster_img)))
-    std_intensities.append(float(np.std(raster_img)))
-
-# add mean and standard deviation of intensities
-unique_DHS_clusters_Africa_intensity = unique_DHS_clusters_Africa
-unique_DHS_clusters_Africa_intensity['mean_intensity'] = mean_intensities
-unique_DHS_clusters_Africa_intensity['std_intensity'] = std_intensities
-
-unique_DHS_clusters_Africa_intensity.to_csv(data_dir + r'/InfantMortality_Cluster5year_intensities.csv')
-
-test = pd.read_csv(data_dir + r'/InfantMortality_Cluster5year_intensities.csv')
-print(test)
+# for idx, cluster in unique_DHS_clusters_Africa.iterrows():
+#     # print(cluster)
+#     raster_img = geo_prop.get_coord_centered_img(cluster['lat'], cluster['lon'], 5, 5, raster,
+#                                                  filepath=data_dir + '/nightlights/' + cluster['cluster_id'] + '.png')
+#
+#     mean_intensities.append(float(np.mean(raster_img)))
+#     std_intensities.append(float(np.std(raster_img)))
+#
+# # add mean and standard deviation of intensities
+# unique_DHS_clusters_Africa_intensity = unique_DHS_clusters_Africa
+# unique_DHS_clusters_Africa_intensity['mean_intensity'] = mean_intensities
+# unique_DHS_clusters_Africa_intensity['std_intensity'] = std_intensities
+#
+# unique_DHS_clusters_Africa_intensity.to_csv(data_dir + r'/InfantMortality_Cluster5year_intensities.csv')
+#
+# test = pd.read_csv(data_dir + r'/InfantMortality_Cluster5year_intensities.csv')
+# print(test)
