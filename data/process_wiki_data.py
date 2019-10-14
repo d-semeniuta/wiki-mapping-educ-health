ada3c32c3dbfefa0daa04290a718154fed2a7ebd
esfrankel <ericsf@stanford.edu>
2019-10-14 15:38:39 -0700

    holding

--- data/process_wiki_data.py
+++ data/process_wiki_data.py
@@ -3,6 +3,7 @@ import re
 import requests
 import datetime
 import mmap
+import dateutil.parser
 
 import os
 
@@ -32,11 +33,11 @@ def build_csv(lat_lon_df, gsradius = 10000, gslimit = 10):
         "list": "geosearch",
         "action": "query"
     }
-    PARAMS["gslimit"]: gslimit
+    # PARAMS["gslimit"]: gslimit
     PARAMS["gsradius"]: gsradius
 
     columns = ["lat", "lon"]
-    within_distance_string = "Num Articles Within " + gsradius + " Meters of Loc"
+    within_distance_string = "Num Articles Within " + str(gsradius) + " Meters of Loc"
     avg_word_count_string = "Avg Word Count"
     avg_rev_count_string = "Avg Revision Count"
     avg_time_since_last_rev_string = "Avg Time Since Last Revision"
@@ -45,30 +46,33 @@ def build_csv(lat_lon_df, gsradius = 10000, gslimit = 10):
     columns.append(within_distance_string)
     columns.append(avg_word_count_string)
     columns.append(avg_rev_count_string)
-    columns.append(avg_time_since_last_rev_string)
+    # columns.append(avg_time_since_last_rev_string)
     columns.append(educ_article_count_string)
     columns.append(health_article_count_string)
 
     compiled_csv = pd.DataFrame(columns=columns)
 
     for index, row in lat_lon_df.iterrows():
+        if index % 1000 == 0:
+            print("Finished " + str(index) + " of " + str(lat_lon_df.shape[0]) + " rows.")
         lat = row["lat"]
         lon = row["lon"]
+        print(lat, lon)
         PARAMS["gscoord"] = str(lat) + "|" + str(lon)
         R = S.get(url=URL, params=PARAMS)
         DATA = R.json()
-
+        # print(DATA)
         PLACES = DATA['query']['geosearch']
         PAGES = []
         PAGE_TITLES = []
 
         for place in PLACES:
-            PAGES.append(place["pageid"])
+            PAGES.append(str(place["pageid"]))
             PAGE_TITLES.append(place["title"])
 
         avg_word_count = find_avg_word_counts(PAGES)
         avg_rev_count = find_avg_revisions(PAGES)
-        avg_time_since_last_rev = find_avg_time_since_last_rev(PAGES)
+        # avg_time_since_last_rev = find_avg_time_since_last_rev(PAGES)
         educ_article_count = education_category_count(PAGE_TITLES)
         health_article_count = health_category_count(PAGE_TITLES)
         compiled_csv = compiled_csv.append({
@@ -77,14 +81,16 @@ def build_csv(lat_lon_df, gsradius = 10000, gslimit = 10):
             within_distance_string: len(PLACES),
             avg_word_count_string: avg_word_count,
             avg_rev_count_string: avg_rev_count,
-            avg_time_since_last_rev_string: avg_time_since_last_rev,
+            # avg_time_since_last_rev_string: avg_time_since_last_rev,
             educ_article_count_string: educ_article_count,
-            health_article_count_string: health_article_count,
-        })
+            health_article_count_string: health_article_count
+        }, ignore_index=True)
 
     lat_lon_df.to_csv(os.path.join(processed_dir, 'geolocation_stats.csv'), index=False)
 
 def find_avg_word_counts(page_id_list):
+    if len(page_id_list) == 0:
+        return 0
     pageids = "|".join(page_id_list)
     PARAMS = {
         "format": "json",
@@ -98,13 +104,18 @@ def find_avg_word_counts(page_id_list):
 
     PAGES = DATA['query']['pages']
     counts = []
+    print(PAGES)
     for page in PAGES:
-        line = page["extract"]
+        if "extract" in PAGES[page].keys():
+        print(page)
+        line = PAGES[page]["extract"]
         count = len(re.findall(r'\w+', line))
         counts.append(count)
     return sum(counts)/len(counts)
 
 def find_avg_revisions(page_id_list):
+    if len(page_id_list) == 0:
+        return 0
     pageids = "|".join(page_id_list)
     PARAMS = {
         "format": "json",
@@ -116,13 +127,13 @@ def find_avg_revisions(page_id_list):
         "rvlimit": "max"
     }
 
-    wp_call = requests.get(BASE_URL, params=parameters)
+    wp_call = requests.get(URL, params=PARAMS)
     response = wp_call.json()
 
     total_revisions = 0
 
     while True:
-      wp_call = requests.get(BASE_URL, params=parameters)
+      wp_call = requests.get(URL, params=PARAMS)
       response = wp_call.json()
       for page_id in response['query']['pages']:
         total_revisions += len(response['query']['pages'][page_id]['revisions'])
@@ -134,6 +145,8 @@ def find_avg_revisions(page_id_list):
     return (total_revisions/(len(page_id_list)))
 
 def education_category_count(page_titles_list):
+    if len(page_titles_list) == 0:
+        return 0
     count = 0
     educ_file_list = ["college", "institute", "library", "school", "university"]
     for title in page_titles_list:
@@ -145,10 +158,12 @@ def education_category_count(page_titles_list):
     return count
 
 def health_category_count(page_titles_list):
+    if len(page_titles_list) == 0:
+        return 0
     count = 0
     health_file_list = ["hospital"]
     for title in page_titles_list:
-        for file in educ_file_list:
+        for file in health_file_list:
             path = os.path.join(data_dir, COORD_ART_PREFIX + file + COORD_ART_SUFFIX)
             if find_string_in_file(path, title):
                 count += 1
@@ -160,29 +175,31 @@ def find_string_in_file(path, target):
         if s.find(str.encode(target)) != -1:
             return True
 
-def find_avg_time_since_last_rev(page_id_list):
-    pageids = "|".join(page_id_list)
-    PARAMS = {
-        "action": "query",
-        "prop": "revisions",
-        "pageids": pageids,
-        "rvprop": "timestamp",
-        "rvslots": "main",
-        "formatversion": "2",
-        "format": "json"
-    }
-
-    R = S.get(url=URL, params=PARAMS)
-    DATA = R.json()
-
-    PAGES = DATA['query']['pages']
-    timestamps = []
-    for page in pages:
-        timestamp = page["timestamp"]
-        dt = datetime.fromisoformat(timestamp)
-        timestamps.append(unix_time_millis(dt))
-
-    return sum(timestamps)/len(timestamps)
+# def find_avg_time_since_last_rev(page_id_list):
+#     if len(page_id_list) == 0:
+#         return 0
+#     pageids = "|".join(page_id_list)
+#     PARAMS = {
+#         "action": "query",
+#         "prop": "revisions",
+#         "pageids": pageids,
+#         "rvprop": "timestamp",
+#         "rvslots": "main",
+#         "formatversion": "2",
+#         "format": "json"
+#     }
+#
+#     R = S.get(url=URL, params=PARAMS)
+#     DATA = R.json()
+#
+#     PAGES = DATA['query']['pages']
+#     timestamps = []
+#     for page in PAGES:
+#         timestamp = page["revisions"][0]["timestamp"]
+#         dt = dateutil.parser.parse(timestamp)
+#         timestamps.append(unix_time_millis(dt))
+#
+#     return sum(timestamps)/len(timestamps)
 
 def unix_time_millis(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
@@ -198,6 +215,7 @@ def main():
     lat_lon_df = lat_lon_df.append(mat_ed[['lat', 'lon']], ignore_index=True)
 
     lat_lon_df.drop_duplicates(keep='first', inplace=True)
+    print(lat_lon_df.head())
     build_csv(lat_lon_df)
 
 if __name__ == '__main__':
