861a335b5e0bc35f3ec6866339f0ccc9741b30b6
esfrankel <ericsf@stanford.edu>
2019-11-10 02:11:15 -0500

    hopefully this resolves issue

--- data/create_doc2vec.py
+++ data/create_doc2vec.py
@@ -9,61 +9,57 @@ import json
 logger = logging.getLogger(__name__)
 
 def build_doc2vec(input_file_path, output_file_path, num_workers):
-    with utils.open(input_file_path, 'rb') as file:
-        for line in file:
-            article = json.loads(line)
-            print(article["title"])
     train_list = []
 
     count = 0
 
-    # with utils.open(input_file_path, 'rb') as file:
-    #     for line in file:
-    #         count+=1
-    #         article = json.loads(line)
-    #         text = article['text']
-    #         title = article['title']
-    #         train_list.append(TaggedDocument(utils.simple_preprocess(doc), [title]))
-    #         if count % 10000 == 0:
-    #             logger.info("Finished processing %d articles.", count)
+    with utils.open(input_file_path, 'rb') as file:
+        for line in file:
+            article = json.loads(line)
+            count += 1
+            text = article['text']
+            title = article['title']
+            train_list.append(TaggedDocument(utils.simple_preprocess(doc), [title]))
+            if count % 10000 == 0:
+                logger.info("Finished processing %d articles.", count)
 
-    # logger.info("Finished processing all of the articles.")
-    # model = Doc2Vec(dm=0, dbow_words=1, vector_size=300, window=8, min_count=15, epochs=10, workers=multiprocessing.cpu_count())
-    #
-    # model.build_vocab(train_list)
-    # logger.info("Vocab built.")
-    #
-    # model.train(train_list, total_examples=model.corpus_count, epochs=model.epochs)
-    # logger.info("Model trained.")
-    #
-    # model.save(output_file_path)
+    logger.info("Finished processing all of the articles.")
+    model = Doc2Vec(dm=0, dbow_words=1, vector_size=300, window=8, min_count=15, epochs=10, workers=multiprocessing.cpu_count())
 
-# def build_vector_dataset(input_file_path, output_file_path, dataset_file_path):
-#     count = 0
-#     with utils.open(input_file_path, 'rb') as file:
-#         model = Doc2Vec.load(output_file_path)
-#         outfile = utils.open(dataset_file_path, 'wb')
-#         for line in file:
-#             count+=1
-#             article = json.loads(line)
-#             title = article["title"]
-#             data_line = {
-#                 "title" : title,
-#                 "coords" : article["coordinates"],
-#                 "embedding" : model.docvecs[title]
-#             }
-#             if count % 10000 == 0:
-#                 logger.info("Finished processing %d articles.", count)
-#
-#             outfile.write((json.dumps(data_line) + "\n").encode('utf-8'))
-#     outfile.close()
+    model.build_vocab(train_list)
+    logger.info("Vocab built.")
+
+    model.train(train_list, total_examples=model.corpus_count, epochs=model.epochs)
+    logger.info("Model trained.")
+
+    model.save(output_file_path)
+
+def build_vector_dataset(input_file_path, output_file_path, dataset_file_path):
+    count = 0
+    with utils.open(input_file_path, 'rb') as file:
+        model = Doc2Vec.load(output_file_path)
+        outfile = utils.open(dataset_file_path, 'wb')
+        for line in file:
+            count+=1
+            article = json.loads(line)
+            title = article["title"]
+            data_line = {
+                "title" : title,
+                "coords" : article["coordinates"],
+                "embedding" : model.docvecs[title]
+            }
+            if count % 10000 == 0:
+                logger.info("Finished processing %d articles.", count)
+
+            outfile.write((json.dumps(data_line) + "\n").encode('utf-8'))
+    outfile.close()
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(module)s - %(levelname)s - %(message)s', level=logging.INFO)
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     default_workers = max(1, multiprocessing.cpu_count() - 1)
     build_doc2vec("./raw/wikipedia/coord_articles_extraced.bz2", "./raw/wikipedia/eric_doc2vec.model", default_workers)
-    # build_vector_dataset("./raw/wikipedia/coord_articles_extraced.bz2", "./raw/wikipedia/eric_doc2vec.model", "./raw/wikipedia/vector_dataest.bz2")
+    build_vector_dataset("./raw/wikipedia/coord_articles_extraced.bz2", "./raw/wikipedia/eric_doc2vec.model", "./raw/wikipedia/vector_dataest.bz2")
     # parser.add_argument('-f', '--file', help='Path to the processed wiki json dump.', required=True)
     # parser.add_argument('-o', '--output', help='Path of the doc2vec model that will be saved.', required=True)
     # parser.add_argument('-d', '--dataset', help='Path of the vector dataset that will be created.', required = True)
