9e1e482aa85f7e0c7160f8d66a202940b33855c0
esfrankel <ericsf@stanford.edu>
2019-12-02 21:57:37 -0800

    we don't have plot preds rn

--- train.py
+++ train.py
@@ -270,7 +270,7 @@ def train_loop(args, params):
                 if not os.path.exists(plot_info['save_dir']):
                     os.makedirs(plot_info['save_dir'])
                 (corrs, losses), (ins, outs) = evaluate(models, val_loader, training_dict['loss_fns'], params)
-                plotPreds(ins, outs, corrs, plot_info)
+                # plotPreds(ins, outs, corrs, plot_info)
                 log_file.write('Validated in {}\n'.format(val))
                 log_file.write('Separate Model - IMR corr: {:.3f}\t\tMatEd corr: {:.3f}\n'.format(corrs['imr'], corrs['mated']))
                 print('\tValidated in {}'.format(val))
